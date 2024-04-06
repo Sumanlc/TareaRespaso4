ceeb8f95f3b2ca0661af015e382ed83f3ebb1a72
sumanlc <sumlam@alu.edu.gva.es>
2024-04-06 14:56:53 +0200

    test 1-2

---
+++ src/es/iesmz/tests/VectorTest.java
@@ -0,0 +1,23 @@
+package es.iesmz.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static es.iesmz.tests.Vector.reverso;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+    @Test
+    public void testReverso1() {
+        int[] v = {1, 2, 3, 4, 5, 6};
+        int[] expected = {6, 5, 4, 3, 2, 1};
+        int[] reversed = reverso(v.clone());
+        assertArrayEquals(expected, reversed);
+    }
+    @Test
+    public void testReverso2() {
+        int[] v = {10, 12, 5, 221, 6, 7};
+        int[] expected = {7, 6, 221, 5, 12, 10};
+        assertArrayEquals(expected, reverso(v)); // JUnit assertion
+    }
+
+}
